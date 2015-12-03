7986b8a95d890e04a433ed819d997633650d6d29
ajohn2550 <alex@johnsonwebware.com>
2015-12-02 22:34:40 -0700

    Alot of work done.....

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
 var pwaCrypt = require('./lib/pwaCrypt');
+
+var crtName = 'pwArch';
+
+var encrypted = pwaCrypt.encrypt('122322', crtName);
+console.log(encrypted);
+var decrypted = pwaCrypt.decrypt(encrypted, crtName);
+console.log(decrypted);
---
+++ test/pwaCrypt/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var expect = require('chai').expect,
+		pwaCrypt = require('../../lib/pwaCrypt'),
+		crtName = 'pwArch';
+
+var testData = {
+	user: 'user',
+	site: 'site',
+	pin: 'pin',
+	crtName: 'pwArch',
+	pass: '122322'
+}
+
+var encrypted = pwaCrypt.encrypt('122322', crtName);
+var decrypted = pwaCrypt.decrypt(encrypted, crtName);
+
+describe('pwaCrypt', function () {
+	it('Should be able to encrypt a string', function (done) {
+		expect(encrypted, 'encryption').to.not.equal('122322');
+		
+		done();
+	});
+
+	it('Should be able to decrypt a string', function (done) {
+		expect(decrypted, 'decryption').to.equal('122322');
+
+		done();
+	});
+
+	it('Should be able to store an encrypted file', function (done) {
+		pwaCrypt.store(testData.user, testData.site, testData.pin, testData.crtName, testData.pass, function (err, success) {
+			expect(err, 'err').to.not.exist;
+			console.error(err);
+			expect(success).to.equal('Password saved');
+			console.info(success);
+
+			done();
+		});
+	});
+
+	it('Should be able to fetch an encrypted password', function (done) {
+		var stored = pwaCrypt.fetch(testData.user, testData.site, testData.pin, testData.crtName);
+		expect(stored, 'fetched password').to.equal(testData.pass);
+
+		done();
+	});
+});
